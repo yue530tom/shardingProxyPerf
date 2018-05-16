4afe746864f4e1b40db49f51b95647f79978fdbd
ling.yue <yue530tom@163.com>
2018-05-16 14:45:18 +0800

    add db schema

---
+++ shardingProxyPerf/relatedDoc/demo_ds_1.sql
@@ -0,0 +1,148 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 10.255.254.41
+Source Server Version : 50713
+Source Host           : 10.255.254.41:3306
+Source Database       : demo_ds_1
+
+Target Server Type    : MYSQL
+Target Server Version : 50713
+File Encoding         : 65001
+
+Date: 2018-05-15 11:27:29
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `t_order_0`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_0`;
+CREATE TABLE `t_order_0` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_0
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_order_1`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_1`;
+CREATE TABLE `t_order_1` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=54007476090 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_1
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_order_2`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_2`;
+CREATE TABLE `t_order_2` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_2
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_order_3`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_3`;
+CREATE TABLE `t_order_3` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=54007476092 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_3
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_order_item_0`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_item_0`;
+CREATE TABLE `t_order_item_0` (
+  `order_item_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_item_0
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_order_item_1`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_item_1`;
+CREATE TABLE `t_order_item_1` (
+  `order_item_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_item_1
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_order_item_2`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_item_2`;
+CREATE TABLE `t_order_item_2` (
+  `order_item_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_item_2
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_order_item_3`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_item_3`;
+CREATE TABLE `t_order_item_3` (
+  `order_item_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_order_item_3
+-- ----------------------------
